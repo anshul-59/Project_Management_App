11270861bdbd7252a0d95c6d6293148afb7d170a
anshul-59 <anshul.bansod5901@gmail.com>
2023-12-28 17:44:05 +0530

    Linking the two buttons that open the terminal to add project using usestate.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSideBar from "./components/ProjectsSideBar.jsx";
 
 function App() {
+
+  const[projectsState, setProjectState] = useState({
+    selectedProjectId:undefined, 
+    projects:[]
+  });
+
+  function handleStartAddProject () {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId:null
+      }
+    })
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject></NewProject> 
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}></NoProjectSelected>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar />
-      <NewProject />
+      <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
